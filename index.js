ecea25f2396803fed2c16284dff1d47558cc594c
JonathanJacobson <jonathanjac@gmail.com>
2023-10-27 14:00:00 +0200

    more

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ x = 10;
 y = 22;
 age = x + y;
 window.alert(yourName + " age is: " + age);
+
+document.getElementById("p1").innerHTML = "Hello, " + yourName;
+document.getElementById("p2").innerHTML = "You are, " + age;
+document.getElementById("p3").innerHTML = "Hello, " + x;
