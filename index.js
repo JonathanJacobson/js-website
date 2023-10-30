134907270c9dfd7dd521d09385370abe93eca9a0
JonathanJacobson <jonathanjac@gmail.com>
2023-10-30 11:26:10 +0200

    new javascript

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-console.log("I love golf");
+//console.log("I love golf");
 
-let age;
-let x, y;
+/*
+var age;
+var x, y;
 let yourName = "Jono";
 
-x = 10;
-y = 22;
-age = x + y;
+yourName = window.prompt("What is your name?");
+
+x = window.prompt("Give me ONE number?");
+y = window.prompt("Give me ANOTHER number?");
+age = parseInt(x) * parseInt(y);
 window.alert(yourName + " age is: " + age);
 
 document.getElementById("p1").innerHTML = "Hello, " + yourName;
 document.getElementById("p2").innerHTML = "You are, " + age;
 document.getElementById("p3").innerHTML = "Hello, " + x;
+*/
+
+let username;
+
+document.getElementById("myButton").onclick = function() {
+    username = document.getElementById("myText").value;
+    document.getElementById("myLabel").innerHTML = "Hello " + username;
+}
