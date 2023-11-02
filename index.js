2bc561f76c4fc2c65ee10ba2201d38c3ec8b45c7
JonathanJacobson <jonathanjac@gmail.com>
2023-11-02 10:01:55 +0200

    count button +=1 ;

--- index.js
+++ index.js
@@ -71,6 +71,20 @@ document.getElementById("solveHypotenuseButton").onclick = function() {
     document.getElementById("sideC").innerHTML = "Answer Side C " + side3;
 }
 
+let count = 0;
+document.getElementById("decreaseButton").onclick = function() {
+    count-=1;
+    document.getElementById("Num").innerHTML = "" + count;
+}
+document.getElementById("resetButton").onclick = function() {
+    count = 0;
+    document.getElementById("Num").innerHTML = "" + count;
+}
+document.getElementById("increaseButton").onclick = function() {
+    count+=1;
+    document.getElementById("Num").innerHTML = "" + count;
+}
+
 function myFunction (a, b)
 {
     x = a * b;
